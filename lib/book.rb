31d98bd93485bd028386de540cf94f80345a0a54
Lindsay Mecher <lindsaymecher@gmail.com>
2020-01-08 16:27:38 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
 
+require_relative "../lib/book.rb"
 
+class Book
+    attr_reader :title, :author, :page_count, :genre, :turn_page
+    attr_writer :title, :author, :page_count, :genre, :turn_page
+    def initialize (title)
+        @title = title
+    end
+
+    def title
+        @title
+    end
+
+    def author
+        @author = "Agatha Christie"
+    end
+
+    def turn_page
+        @string = puts "Flipping the page...wow, you read fast!"
+    end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,35 @@
 # Make your shoe class here!
+
+class Shoe
+    attr_reader :brand, :color, :size, :material, :condition
+    attr_writer :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+    def shoe
+        @shoe
+    end
+    def brand
+        @brand
+    end
+    def color
+        @color
+    end
+    def size
+        @size
+    end
+    def material
+        @material
+    end
+    def condition
+        @condition
+    end
+    def cobble
+        if condition = "old"
+            self.condition = "new"
+        end
+        puts "Your shoe is as good as new!"
+    end
+    
+end
